82d6c9355fb800005a20c1c98c92d3b699835d8f
Pratham kothari <en20it301079@medicaps.ac.in>
2025-01-13 15:56:15 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import { Route, Router, Routes } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import Create from './pages/Create'
+import NavBar from './Components/NavBar'
+
+
+function App() {
+
+  const [toggle , setToggle] = useState("white")
+
+  const setTheme = () =>{
+    console.log("here it is ");
+    
+     setToggle((prevToggle)=> (prevToggle ==="white" ? "black" : "white"))
+  }
+
+
+  return (
+    <div className={`${toggle==="white" ? "bg-white text-black" : "bg-black text-white"} min-h-screen`}>
+    <NavBar toggle={toggle} setTheme={setTheme}/>
+    <Routes>
+    
+       <Route path="/" element={<HomePage/>} />
+       <Route path='/create' element={<Create/>} />
+    </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function HomePage() {
+  return (
+    <div>
+
+    HomePage
+    <div>
+        this line is weitten in home page
+    
+         </div>
+
+    <div>
+        epfvojopdfjv
+    </div>
+      
+    </div>
+  )
+}
